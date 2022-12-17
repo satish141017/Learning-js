3a0a5586dbfc01dbbca7843bde91afcb946f7d74
Satish Kumar <110656011+satish141017@users.noreply.github.com>
2022-12-17 12:11:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let k=prompt("enter your name :")
+let l=prompt("enter your age :")
+document.getElementById("p1").innerHTML="MY";
+document.getElementById("span1").innerHTML="NAME";
+document.getElementById("span2").innerHTML="IS";
+document.getElementById("name").innerHTML=k;
+document.getElementById("p2").innerHTML="MY"
+document.getElementById("span3").innerHTML="AGE";
+document.getElementById("span4").innerHTML="IS";
+document.getElementById("age").innerHTML=l;
+
+
